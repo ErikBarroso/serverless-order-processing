05153c61635738e312e372be24a60b4eefd3804b
Erik Barroso <coderik1996@gmail.com>
2025-02-27 08:03:06 -0300

    feat: add docker file

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,13 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 
-const PORT = process.env.PORT;
+const PORT = parseInt(process.env.PORT || '3000') ;
 
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Order API is active!');
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running at PORT: ${PORT}`);
 });
 
