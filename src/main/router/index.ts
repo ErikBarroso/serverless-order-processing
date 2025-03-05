3234a5dedadd4e73b4f83b709c27e8230174f2ab
ErikBarroso <coderik1996@gmail.com>
2025-03-05 12:16:08 -0300

    feat: add router login

--- src/main/router/index.ts
+++ src/main/router/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import productRouter from './product';
 import orderRouter from './order';
 import userRouter from './user';
+import authRouter from './auth';
 
 const router = Router();
 
 router.use('/products', productRouter);
 router.use('/orders', orderRouter);
 router.use('/users', userRouter);
+router.use('/login', authRouter);
 
 export default router;
