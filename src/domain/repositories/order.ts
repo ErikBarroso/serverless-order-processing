35d6d0cf8f84139a3d9ce53f241a22944f8f0ca5
ErikBarroso <coderik1996@gmail.com>
2025-03-05 12:18:04 -0300

    refactor: change types

--- src/domain/repositories/order.ts
+++ src/domain/repositories/order.ts
@@ -4,5 +4,5 @@ export interface OrderRepository {
     create(item: Order): Promise<Order>;
     delete(id: string, userId: string): Promise<void>;
     findById(id: string, userId: string): Promise<Order | null>;
-    findAll(userId: string): Promise<Order[] | []>;
+    findAll(userId: string): Promise<Order[]>;
 }
--- src/domain/repositories/product.ts
+++ src/domain/repositories/product.ts
@@ -4,5 +4,5 @@ export interface ProductRepository {
     create(item: Product): Promise<Product>;
     delete(id: string): Promise<void>;
     findById(id: string): Promise<Product | null>;
-    findAll(): Promise<Product[] | []>;
+    findAll(): Promise<Product[]>;
 }
