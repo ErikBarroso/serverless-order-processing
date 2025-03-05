c8116cbfa2e00c1407913bbb832f04b0b67c9191
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-05 17:53:35 -0300

    feat: add hash in bcrypt

--- src/infrastructure/encrypters/bcrypt.ts
+++ src/infrastructure/encrypters/bcrypt.ts
@@ -1,7 +1,24 @@
 import bcrypt from 'bcryptjs';
 import { Encrypter } from '../../data/utils/encrypter';
 
-export class Bcrypt implements Encrypter {
+export default class Bcrypt implements Encrypter {
+  async hash(value: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+      const saltRounds = 10;
+      bcrypt.genSalt(saltRounds, (errSalt, salt) => {
+        if (errSalt) {
+          reject(errSalt);
+        }
+        bcrypt.hash(value, salt, (errHash, hash) => {
+          if (errHash) {
+            reject(errHash);
+          }
+          resolve(hash);
+        });
+      });
+    });
+  }
+
   async compare(value: string, hash: string): Promise<boolean> {
     return new Promise((resolve, reject) => {
       bcrypt.compare(value, hash, (err, result) => {
