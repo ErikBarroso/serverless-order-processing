35c737d85a151467c806c9e0c59e6b9dfa2368e1
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-05 17:52:58 -0300

    refactor: LoginUseCase

--- src/data/useCases/auth/login.ts
+++ src/data/useCases/auth/login.ts
@@ -2,33 +2,38 @@ import { UserRepository } from '../../../domain/repositories/user';
 import { LoginUseCase, LoginUseCaseResult } from '../../../domain/use-cases/auth/login';
 import { Encrypter } from '../../utils/encrypter';
 import { JWT } from '../../utils/jwt';
-import { JwtConfig } from '../../utils/jwt-config';
-import { unauthorized } from '../../utils/result';
+import { ok, unauthorized } from '../../utils/result';
 
 export class LoginUseCaseImpl implements LoginUseCase {
   public constructor(
     private readonly userRepository: UserRepository,
-    private readonly passwordEncrypter: Encrypter,
     private readonly jwt: JWT,
-    private readonly jwtConfig: JwtConfig,
-    private readonly refreshTokenEncrypter: Encrypter,
+    private readonly passwordEncrypter: Encrypter,
   ) {}
 
-  async exec(email: string, pawword: string): Promise<LoginUseCaseResult> {
+  async exec(email: string, password: string): Promise<LoginUseCaseResult> {
     const user = await this.userRepository.findByIdOrEmail(email);
     if (!user) {
       return unauthorized('Email ou senha inválido');
     }
-    const isPasswordValid = await this.passwordEncrypter.compare(pawword, user.password);
+
+    const isPasswordValid = await this.passwordEncrypter.compare(password, user.password);
     if (!isPasswordValid) {
       return unauthorized('Email ou senha inválido');
     }
+
     const accessToken = await this.jwt.sign({
-      id: user.id,
-    },this.jwtConfig);
-    const refreshToken: string = await this.refreshTokenEncrypter.hash(
-      this.hashPadding + accessToken + this.hashPadding,
-    );
-    // ver o lance do rregreshtoken
+      id: user.id }, 
+    process.env.JWT_SECRET || 'defaultKey', 
+    parseInt(process.env.JWT_EXPIRES_IN as string) || 60);
+    const result: LoginUseCaseResult = {
+      accessToken,
+      user : {
+        id: user.id,
+        email: user.email,
+        name: user.name,
+      },
+    };
+    return ok('Ação realizada com sucesso!', result);
   }
 }
--- src/domain/use-cases/auth/login.ts
+++ src/domain/use-cases/auth/login.ts
@@ -6,10 +6,9 @@ export type UserLogin = {
 
 export type LoginUseCaseResult = {
     accessToken: string;
-    refreshToken: string;
     user: UserLogin;
 }
 
 export interface LoginUseCase {
-    exec(email: string, pawword: string): Promise<LoginUseCaseResult>
+    exec(email: string, password: string): Promise<LoginUseCaseResult>
 }
