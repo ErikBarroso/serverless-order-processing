63d98cdafbbfea747f5168f77f7f3bdb28068def
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-07 18:23:30 -0300

    refactor: router users

--- src/main/router/user.ts
+++ src/main/router/user.ts
@@ -1,89 +1,16 @@
-import { randomUUID } from 'crypto';
 import { Router } from 'express';
-import AWS from 'aws-sdk';
-import Bcrypt from '../../infrastructure/encrypters/bcrypt';
+import adaptRoute from '../adpters/express-route-adpter';
+import createUser from '../factories/controllers/user/create-user';
+import findUsers from '../factories/controllers/user/find-users';
+import authorizator from '../factories/middlewares/authorizator';
+import adaptMiddleware from '../adpters/express-middleware-adpter';
 
-AWS.config.update({
-  region:'us-east-1',
-  accessKeyId: 'fakeAccessKeyId', // Chave falsa
-  secretAccessKey: 'fakeSecretAccessKey', // Chave falsa
-  logger: console, 
-});
-
-const docClient = new AWS.DynamoDB.DocumentClient({
-  endpoint: 'http://db:4566',
-});
-//refactor essa rota
 const userRouter  = Router();
 
-const bcrypt = new Bcrypt();
-
-//creat
-userRouter.post('/', async (req, res) => {
-  const { name, email, password } = req.body;
-  
-  try {
-    const hashedPassword = await bcrypt.hash(password);  
-  
-    const params = {
-      TableName: 'Users',
-      Item: {
-        id: randomUUID(),
-        name,
-        email,
-        password: hashedPassword,  
-      },
-    };
-  
-    docClient.put(params, (err, data) => {
-      if (err) {
-        console.error('DynamoDB Error:', err);
-        return res.status(500).json({ error: 'Erro ao adicionar usuário', details: err });
-      }
-      res.status(201).json({ message: 'Usuário adicionado com sucesso', data });
-    });
-  
-  } catch (error) {
-    console.error('Erro ao criar hash da senha:', error);
-    res.status(500).json({ error: 'Erro ao processar a senha' });
-  }
-});
-
-//delet
-
-userRouter.delete('/:id', (req,res) => {
-  const id = req.params.id;
-  docClient.delete({ Key: { id }, TableName: 'Users' }, (err, data) => {
-    // tratamento para buscar o item no banco antes de mandar deletar
-    if (err) {
-      console.error('DynamoDB Error:', err);
-      return res.status(500).json({ error: 'erro ao deletar user', details: err });
-    }
-    res.status(200).json({ message: 'user deletado com sucesso' });
-  });
-});
-
-// get item
-userRouter.get('/:id', (req,res) => {
-  const id = req.params.id;
-  docClient.get({ Key: { id }, TableName: 'Users' }, (err, data) => {
-    if (err) {
-      console.error('DynamoDB Error:', err);
-      return res.status(500).json({ error: 'erro ao buscar user', details: err });
-    }
-    res.status(200).json({ data });
-  });
-});
-
-// get items all
-userRouter.get('/', (req,res) => {
-  docClient.scan({ TableName: 'Users' }, (err, data) => {
-    if (err) {
-      console.error('DynamoDB Error:', err);
-      return res.status(500).json({ error: 'erro buscar users', details: err });
-    }
-    res.status(200).json({ data });
-  });
-});
+userRouter.post('/', adaptRoute(createUser));
+userRouter.get('/', 
+  adaptMiddleware(authorizator),
+  adaptRoute(findUsers),
+);
 
 export default userRouter;
