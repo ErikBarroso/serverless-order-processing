88b0855546dd17354e03830f1b68b64655bc46e0
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-08 21:20:21 -0300

    feat: add notFoundItems

--- src/data/use-cases/order/create-orders.ts
+++ src/data/use-cases/order/create-orders.ts
@@ -2,15 +2,26 @@ import { randomUUID } from 'crypto';
 import { Order, OrderStatus } from '../../../domain/entities/order';
 import { OrderRepository } from '../../../domain/repositories/order';
 import { CreateOrderUseCase } from '../../../domain/use-cases/order/create-order';
-import { created } from '../../utils/result';
+import { created, notFound } from '../../utils/result';
 import { Product } from '../../../domain/entities/product';
+import { ProductRepository } from '../../../domain/repositories/product';
 
 export class CreateOrderUseCaseImpl implements CreateOrderUseCase {
-  constructor(private readonly repo: OrderRepository) {}
+  constructor(
+    private readonly ordersRepo: OrderRepository,
+    private readonly productRepo: ProductRepository,
+  ) {}
 
   async exec(items: Product['id'][], customerId: string): Promise<Order> {
-    // buscar o user
-    // buscar os items para ver se existem = produtos
+    const productsDb = await this.productRepo.findAll();
+    const productsIds = new Set(productsDb.map(product => product.id));
+
+    const notFoundItems  = items.filter(itemId => !productsIds.has(itemId));
+    if (notFoundItems.length > 0) {
+      return notFound('Produtos não encontrados!');
+    }
+    
+    // verificar se os meus items existem no products
     const order: Order = {
       id: randomUUID(),
       customerId,
@@ -19,7 +30,7 @@ export class CreateOrderUseCaseImpl implements CreateOrderUseCase {
       createdAt: new Date().toISOString(),
     };
 
-    const result = await this.repo.create(order);
+    const result = await this.ordersRepo.create(order);
     return created('Pedido criado com sucesso!', result);
   }
 }
