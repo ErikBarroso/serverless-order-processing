3e400352edb316fb13729ddf6e93877462ee9828
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-09 18:49:54 -0300

    test: adjust tests

--- tests/unit/use-cases/orders/create-order.test.ts
+++ tests/unit/use-cases/orders/create-order.test.ts
@@ -2,27 +2,32 @@ import { CreateOrderUseCaseImpl } from '../../../../src/data/use-cases/order/cre
 import { created } from '../../../../src/data/utils/result';
 import mockedOrder from '../../../utils/mocks/order';
 import { DynamoOrderRepositoryStub } from '../../../utils/stubs/repositories/dynamo/order';
+import { DynamoProductRepositoryStub } from '../../../utils/stubs/repositories/dynamo/product';
 
 interface SutTypes {
     sut: CreateOrderUseCaseImpl
     dynamoOrderRepositoryStub : DynamoOrderRepositoryStub
+    dynamoProductRepositoryStub : DynamoProductRepositoryStub
 }
 
 const makeSut = (): SutTypes => {
   const dynamoOrderRepositoryStub = new DynamoOrderRepositoryStub();
+  const dynamoProductRepositoryStub = new DynamoProductRepositoryStub();
   const sut = new CreateOrderUseCaseImpl(
     dynamoOrderRepositoryStub,
+    dynamoProductRepositoryStub,
   );
   return {
     sut,
     dynamoOrderRepositoryStub,
+    dynamoProductRepositoryStub,
   };
 };
 
 describe('CreateOrderUseCaseImpl', () => {
   it('should return 201 if an order is created successfully', async () => {
     const { sut } = makeSut();
-    const res = await sut.exec(['id-01', 'id-02'], 'customerId');
+    const res = await sut.exec(['id'], 'customerId');
 
     expect(res).toEqual(created('Pedido criado com sucesso!', mockedOrder));
   });
