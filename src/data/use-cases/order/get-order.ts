0daa1aceb81d59b5aff09f5297663fdbf5ebbbca
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-11 17:14:10 -0300

    feat: add rule

--- src/data/use-cases/order/get-order.ts
+++ src/data/use-cases/order/get-order.ts
@@ -1,13 +1,16 @@
 import { Order } from '../../../domain/entities/order';
 import { OrderRepository } from '../../../domain/repositories/order';
 import { GetOrderByIdUseCase } from '../../../domain/use-cases/order/get-order-by-id';
-import { ok } from '../../utils/result';
+import { notFound, ok } from '../../utils/result';
 
 export class GetOrderByIdUseCaseImpl implements GetOrderByIdUseCase {
   constructor(private readonly repo: OrderRepository) {}
 
   async exec(id: Order['id'], userId: string): Promise<Order> {
     const order = await this.repo.findById(id, userId);
+    if (!order) {
+      return notFound('Pedido não encontrado');
+    }
     return ok('Ação realizada com sucesso!', order);
   }
 }
