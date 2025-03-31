ecf8b19f4d2eb59b4a08c1e1f257d99c4e6e499f
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-03-31 16:29:40 -0300

    feat: add rateLimit

--- src/main/router/auth.ts
+++ src/main/router/auth.ts
@@ -3,10 +3,22 @@ import adaptValidationMiddleware from '../adpters/validate-schema';
 import { loginSchema } from '../../presentation/helpers/login-validator';
 import adaptRoute from '../adpters/express-route-adpter';
 import login from '../factories/controllers/auth/login';
-
+import rateLimit from 'express-rate-limit';
 
 const authRouter = Router();
 
-authRouter.post('/', adaptValidationMiddleware(loginSchema), adaptRoute(login));
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000,
+  limit: 15,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler(req, res /* next */) {
+    res.status(429).json({
+      message: 'Muitas requisições! Tente novamente mais tarde.',
+    });
+  },
+});
+
+authRouter.post('/', limiter, adaptValidationMiddleware(loginSchema), adaptRoute(login));
 
 export default authRouter;
