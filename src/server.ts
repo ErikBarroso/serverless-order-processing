33bb45f006c006d9e5cfaef6d4756b688228f877
Erik Barroso <117813798+ErikBarroso@users.noreply.github.com>
2025-06-02 19:45:48 -0300

    refactor: removing ec2 localstack

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ require('newrelic');
 import express, { Response, Request } from 'express';
 import dotenv from 'dotenv';
 import router from './main/router/index';
-import { checkEc2InstanceStatus } from './data/services/ec2-services';
 import { QueueProcessorWorker } from './infrastructure/workers/queue-processor';
 
 
@@ -18,19 +17,6 @@ app.use('/api', router);
 app.get('/', (req: Request, res: Response) => {
   res.status(200).send('Order API is active!');
 });
-
-async function checkServerStartMode(): Promise<void> {
-  if (process.env.NODE_ENV === 'development') {
-    const isEc2Running = await checkEc2InstanceStatus();
-    if (!isEc2Running) {
-      return console.error('A instância EC2 não está ativa.');
-    }
-  
-    return startAppServer();
-  }
-
-  startAppServer();
-}
 
 function startAppServer(): void {
   app.listen(PORT, '0.0.0.0', () => {
@@ -71,4 +57,4 @@ function stopServerGracefully(signal: string, workerFromQueueSqs: QueueProcessor
   process.exit(0);
 }
 
-checkServerStartMode();
+startAppServer();
